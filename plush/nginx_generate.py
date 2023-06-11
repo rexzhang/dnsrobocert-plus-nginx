925d4e3694e929ac59ae50998be396e187fba91e
Rex Zhang <rex.zhang@gmail.com>
2023-06-11 17:43:09 +0800

    Add generate stream.d conf

--- plush/nginx_generate.py
+++ plush/nginx_generate.py
@@ -36,52 +36,52 @@ SNIPPET_TEMPLATE_CLIENT_MAX_BODY_SIZE = """
         client_max_body_size $client_max_body_size;
 """
 
-httpd_template_main_http_and_https = """
+http_d_template_main_only_http = """
 server {
-    listen $listen_http;
-    listen [::]:$listen_http;
+    listen $listen;
+    listen [::]:$listen;
+    server_name $server_name;
+    
+    set $$upstream $upstream;
+    
+    $locations
+}"""
+
+http_d_template_main_only_https = """
+server {
+    listen $listen_ssl ssl http2;
+    listen [::]:$listen_ssl ssl http2;
+    server_name $server_name;
+
+    set $$upstream $upstream;
+
+    $ssl_params
+    $locations
+}"""
+
+http_d_template_main_http_and_https = """
+server {
+    listen $listen;
+    listen [::]:$listen;
     server_name $server_name;
 
     return 301 https://$server_name$$request_uri;
 }
 
 server {
-    listen $listen_https ssl http2;
-    listen [::]:$listen_https ssl http2;
+    listen $listen_ssl ssl http2;
+    listen [::]:$listen_ssl ssl http2;
     server_name $server_name;
 
     $ssl_params
-    
+
     set $$upstream $upstream;
 
     $locations
 }
 """
 
-httpd_template_main_only_http = """
-server {
-    listen $listen_http;
-    listen [::]:$listen_http;
-    server_name $server_name;
-    
-    set $$upstream $upstream;
-    
-    $locations
-}"""
-
-httpd_template_main_only_https = """
-server {
-    listen $listen_https ssl http2;
-    listen [::]:$listen_https ssl http2;
-    server_name $server_name;
-
-    set $$upstream $upstream;
-
-    $ssl_params
-    $locations
-}"""
-
-httpd_template_location_default = """
+http_d_template_location_default = """
     location / {
         $client_max_body_size
         $proxy_params
@@ -90,10 +90,29 @@ httpd_template_location_default = """
         proxy_pass $$upstream;
     }"""
 
-httpd_template_location_custom = """
+http_d_template_location_custom = """
     location $location_path {
         $location_content
     }"""
+
+stream_d_template_main_no_ssl = """
+server {
+    # $comment
+    listen $listen;
+    
+    proxy_pass $upstream;
+}"""
+
+stream_d_template_main_only_ssl = """
+server {
+    # $comment
+    listen $listen_ssl ssl;
+
+    proxy_ssl on;
+    $ssl_params
+
+    proxy_pass $upstream;
+}"""
 
 
 class Default(pydantic.BaseModel):
@@ -101,25 +120,34 @@ class Default(pydantic.BaseModel):
     ssl_key_file: str
 
 
-class HTTPD(pydantic.BaseModel):
+class ServerAbc(pydantic.BaseModel):
     enable: bool = True
-    server_name: str
 
-    listen_http: int = None
-    listen_https: int = None
+    listen: int = None
+    listen_ssl: int = None
 
     ssl_crt_file: str = None
     ssl_key_file: str = None
+
     upstream: str
+
+
+class HTTPD(ServerAbc):
+    server_name: str
 
     location: dict[str, str] = dict()
     client_max_body_size: str = None
     support_websocket: bool = False
 
 
+class StreamD(ServerAbc):
+    comment: str = "---"
+
+
 class Config(pydantic.BaseModel):
     default: Default
-    httpd: list[HTTPD]
+    http_d: list[HTTPD]
+    stream_d: list[StreamD]
 
 
 class NginxGenerator:
@@ -149,21 +177,44 @@ class NginxGenerator:
         self.stream_d_dir = stream_d_dir
 
     def __call__(self, *args, **kwargs):
+        # http.d
         http_d_conf_filename = (
             Path(self.http_d_dir).joinpath(HTTP_D_CONF_FILENAME).as_posix()
         )
-        logger.info(f"Generate httpd file:[{http_d_conf_filename}]...")
-        httpd_conf_str = ""
-        for httpd in self.config.httpd:
-            if httpd.enable:
-                httpd_conf_str += self._generate_one_httpd(httpd)
+        logger.info(f"Generate http.d file:[{http_d_conf_filename}]...")
+        http_d_conf_str = ""
+        for http_d in self.config.http_d:
+            if http_d.enable:
+                http_d_conf_str += self._generate_one_http_d(http_d)
 
         try:
             with open(http_d_conf_filename, "w") as f:
-                f.write(httpd_conf_str)
+                f.write(http_d_conf_str)
             logger.info(f"Generate httpd file:[{http_d_conf_filename}]...DONE")
         except OSError as e:
-            logger.critical(f"Generate httpd file:[{http_d_conf_filename}] failed, {e}")
+            logger.critical(
+                f"Generate http.d file:[{http_d_conf_filename}] failed, {e}"
+            )
+            exit(1)
+
+        # stream.d
+        stream_d_conf_filename = (
+            Path(self.stream_d_dir).joinpath(STREAM_D_CONF_FILENAME).as_posix()
+        )
+        logger.info(f"Generate stream.d file:[{stream_d_conf_filename}]...")
+        stream_d_conf_str = ""
+        for stream_d in self.config.stream_d:
+            if stream_d.enable:
+                stream_d_conf_str += self._generate_one_stream_d(stream_d)
+
+        try:
+            with open(stream_d_conf_filename, "w") as f:
+                f.write(stream_d_conf_str)
+            logger.info(f"Generate stream.d file:[{stream_d_conf_filename}]...DONE")
+        except OSError as e:
+            logger.critical(
+                f"Generate stream.d file:[{stream_d_conf_filename}] failed, {e}"
+            )
             exit(1)
 
     def _generate_ssl_snippet(self, ssl_crt_file: str, ssl_key_file: str) -> str | None:
@@ -182,39 +233,39 @@ class NginxGenerator:
             }
         )
 
-    def _generate_one_httpd(self, httpd: HTTPD) -> str:
-        httpd_str = ""
+    def _generate_one_http_d(self, http_d: HTTPD) -> str:
+        http_d_str = ""
 
         # httpd ssl
-        if not isinstance(httpd.listen_https, int):
+        if not isinstance(http_d.listen_ssl, int):
             ssl_params_str = ""
         else:
             ssl_params_str = self._generate_ssl_snippet(
-                httpd.ssl_crt_file, httpd.ssl_key_file
+                http_d.ssl_crt_file, http_d.ssl_key_file
             )
             if ssl_params_str is None:
                 logger.error(
-                    f"httpd:[{httpd.server_name}] miss [ssl_crt_file] and [ssl_key_file]"
+                    f"http.d:[{http_d.server_name}] miss [ssl_crt_file] and [ssl_key_file]"
                 )
                 return ""
 
         # httpd location
         locations_str = ""
-        if "/" not in httpd.location:
+        if "/" not in http_d.location:
             # create from default template
-            if httpd.client_max_body_size is None:
+            if http_d.client_max_body_size is None:
                 client_max_body_size_str = ""
             else:
                 client_max_body_size_str = Template(
                     SNIPPET_TEMPLATE_CLIENT_MAX_BODY_SIZE
-                ).substitute({"client_max_body_size": httpd.client_max_body_size})
+                ).substitute({"client_max_body_size": http_d.client_max_body_size})
 
-            if httpd.support_websocket:
+            if http_d.support_websocket:
                 support_websocket = SNIPPET_WEBSOCKET
             else:
                 support_websocket = ""
 
-            locations_str += Template(httpd_template_location_default).substitute(
+            locations_str += Template(http_d_template_location_default).substitute(
                 {
                     "proxy_params": SNIPPET_PROXY_PARAMS,
                     "client_max_body_size": client_max_body_size_str,
@@ -222,34 +273,65 @@ class NginxGenerator:
                 }
             )
 
-        for k, v in httpd.location.items():
-            locations_str += Template(httpd_template_location_custom).substitute(
+        for k, v in http_d.location.items():
+            locations_str += Template(http_d_template_location_custom).substitute(
                 {"location_path": k, "location_content": v}
             )
 
         # httpd main
         main_template = None
-        match (isinstance(httpd.listen_http, int), isinstance(httpd.listen_https, int)):
+        match (isinstance(http_d.listen, int), isinstance(http_d.listen_ssl, int)):
             case (False, False):
                 logger.error(
-                    f"httpd:[{httpd.server_name}] miss [listen_http] and [listen_https]"
+                    f"http.d:[{http_d.server_name}] miss [listen] and [listen_ssl]"
                 )
                 return ""
             case (True, False):
-                main_template = Template(httpd_template_main_only_http)
+                main_template = Template(http_d_template_main_only_http)
             case (False, True):
-                main_template = Template(httpd_template_main_only_https)
+                main_template = Template(http_d_template_main_only_https)
             case (True, True):
-                main_template = Template(httpd_template_main_http_and_https)
+                main_template = Template(http_d_template_main_http_and_https)
 
-        httpd_str += main_template.substitute(
+        http_d_str += main_template.substitute(
             {
-                "server_name": httpd.server_name,
-                "listen_http": httpd.listen_http,
-                "listen_https": httpd.listen_https,
+                "server_name": http_d.server_name,
+                "listen": http_d.listen,
+                "listen_ssl": http_d.listen_ssl,
                 "ssl_params": ssl_params_str,
-                "upstream": httpd.upstream,
+                "upstream": http_d.upstream,
                 "locations": locations_str,
             }
         )
-        return httpd_str
+        return http_d_str
+
+    def _generate_one_stream_d(self, stream_d: StreamD) -> str:
+        stream_d_str = ""
+        if stream_d.listen:
+            stream_d_str += Template(stream_d_template_main_no_ssl).substitute(
+                {
+                    "comment": stream_d.comment,
+                    "listen": stream_d.listen,
+                    "upstream": stream_d.upstream,
+                }
+            )
+
+        if stream_d.listen_ssl:
+            ssl_params_str = self._generate_ssl_snippet(
+                stream_d.ssl_crt_file, stream_d.ssl_key_file
+            )
+            if ssl_params_str is None:
+                logger.error(
+                    f"stream.s:[{stream_d.listen_ssl}] miss [ssl_crt_file] and [ssl_key_file]"
+                )
+                return ""
+
+            stream_d_str += Template(stream_d_template_main_only_ssl).substitute(
+                {
+                    "comment": stream_d.comment,
+                    "listen_ssl": stream_d.listen_ssl,
+                    "ssl_params": ssl_params_str,
+                    "upstream": stream_d.upstream,
+                }
+            )
+        return stream_d_str
