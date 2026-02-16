6ae9007fea42dd77edebeeffc3397c59fc9ace72
Rex Zhang <rex.zhang@gmail.com>
2026-02-16 23:45:27 +0800

    refactor(cli): migrate from click to typer for CLI implementation

--- plush/cli.py
+++ plush/cli.py
@@ -1,51 +1,40 @@
 from logging import getLogger
 
-import click
+import typer
 
-from plush.constants import CONFIG_NGINX_TOML, NGINX_CONF_DIR
-from plush.nginx import NginxGenerator
-from plush.worker import ScheduleDaemon
+from .constants import CONFIG_NGINX_TOML, NGINX_CONF_DIR
+from .nginx import NginxGenerator
+from .worker import ScheduleDaemon
 
 logger = getLogger(__name__)
 
+app = typer.Typer()
 
-@click.group()
-def cli(**cli_kwargs):
-    # do something
-    return
-
-
-@cli.group(help="Task Worker")
-def worker(**cli_kwargs):
-    pass
+worker_app = typer.Typer()
+app.add_typer(worker_app, name="worker", help="Task Worker")
 
 
-@worker.command("start", help="Start Worker")
-def worker_start(**cli_kwargs):
+@worker_app.command("start", help="Start Worker")
+def worker_start():
     ScheduleDaemon().start()
 
 
-@worker.command("stop", help="Stop Worker")
-def worker_stop(**cli_kwargs):
+@worker_app.command("stop", help="Stop Worker")
+def worker_stop():
     ScheduleDaemon().stop()
 
 
-@cli.command("generate", help="nginx *.conf generator")
-@click.option(
-    "--config-nginx-toml",
-    default=CONFIG_NGINX_TOML,
-    type=str,
-    help=f"[default:{CONFIG_NGINX_TOML}]",
-)
-@click.option(
-    "--nginx-conf-dir",
-    default=NGINX_CONF_DIR,
-    type=str,
-    help=f"[default:{NGINX_CONF_DIR}]",
-)
-def generator(**cli_kwargs):
-    NginxGenerator(**cli_kwargs)()
+@app.command("generate", help="nginx *.conf generator")
+def generate(
+    config_nginx_toml: str = typer.Option(
+        CONFIG_NGINX_TOML, help=f"[default:{CONFIG_NGINX_TOML}]"
+    ),
+    nginx_conf_dir: str = typer.Option(
+        NGINX_CONF_DIR, help=f"[default:{NGINX_CONF_DIR}]"
+    ),
+):
+    NginxGenerator(config_nginx_toml=config_nginx_toml, nginx_conf_dir=nginx_conf_dir)()
 
 
 def main():
-    cli()
+    app()
